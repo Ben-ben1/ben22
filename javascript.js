68fe0cdcbf2bf1dae437aa0be01483e8a75b7bf5
Ben-ben1 <156182047+Ben-ben1@users.noreply.github.com>
2024-01-23 11:38:36 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-j
+function lit1(){
+    var lit6 = document.getElementById("mm");
+    
+    if (lit6.className === "pap" ){
+    lit6.className = "res";
+   
+    
+
+}
+else{
+    lit6.className = "pap";
+
+}
+
+}
